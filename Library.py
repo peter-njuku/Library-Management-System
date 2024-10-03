e8354dbf183049acebc21ce107a2e463157fe4af
Peter Njuku <njuku2005@gmail.com>
2024-10-03 14:47:14 +0300

    Any changes are accepted

--- Library.py
+++ Library.py
@@ -1,6 +1,6 @@
 import pymysql
 from pymysql import MySQLError
-from datetime import date
+from datetime import datetime
 
 def create_connection():
     try:
@@ -46,23 +46,40 @@ def add_member(connection, first_name,last_name,date_of_birth,address,phone_numb
     except MySQLError as e:
         print("Error encountered when adding new member", e)
         
-def add_transaction(connection, member_id, book_id,transaction_type, ):
+        
+def calculate_fine(return_date,due_date):
+    if return_date and return_date > due_date:
+        overdue_days = (return_date - due_date).days
+        fine = overdue_days * 20
+        return fine
+    return 0
+        
+def add_transaction(connection, member_id, book_id, transaction_type, issue_date, due_date, return_date, transaction_status, notes):
     try:
-        with connection.cursor as cursor:
-            pass
-    
+        fine_amount = calculate_fine(return_date, due_date)
+        with connection.cursor() as cursor:
+            insert_query = """INSERT INTO transactions (member_id, book_id, transaction_type, issue_date, due_date, return_date, fine_amount, transaction_status, notes) 
+                              VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            transaction_data = (member_id, book_id,transaction_type, issue_date,due_date, return_date,fine_amount, transaction_status, notes)
+            cursor.execute(insert_query, transaction_data)
+            
+        connection.commit()
+        print("Transaction recorded successfully")
+        return fine_amount
+        
     except MySQLError as e:
-        pass
+        print("Error encounter when recording the transaction",e)
 
 
 def main():
     connection = create_connection()
     
     while True:
-        print("\nLibrary MAnagement System")
-        print("1. Add a book")
-        print("2. Add a new member")
-        print("3. Exit")
+        print("\nLIBRARY MANAGEMENT SYSTEM")
+        print("1. Add a book.")
+        print("2. Add a new member.")
+        print("3. Record a transaction.")
+        print("4. Exit")
         choice = input("Enter your choice:  ")
         
         
@@ -99,8 +116,35 @@ def main():
             add_member(connection, first_name, last_name, date_of_birth, address, phone_number, email, membership_type, 
                        membership_start_date, membership_expiry_date, membership_status, outstanding_fees, 
                        num_books_borrowed, identification_document)
+         
+        elif choice == "3":
+            member_id = int(input("Enter member ID:  "))
+            book_id = input("Enter book ID:  ")
+            transaction_type = input("Enter transaction type (Issue/Return):  ").capitalize()
+            if transaction_type == "Issue":
+                issue_date = input("Enter issue date (YYYY-MM-DD):  ")
+                due_date = input("To be returned by date(YYYY-MM-DD):  ")
+                return_date = None
+                transaction_status = "Active"
+                notes = input("Enter description: ")
+                add_transaction(connection, member_id, book_id, transaction_type, issue_date, due_date, return_date, transaction_status, notes)
             
-        elif choice == '3':
+            elif transaction_type == "Return":
+                return_date_input = input("Enter return date (YYYY-MM-DD): ")
+                return_date = datetime.strptime(return_date_input, "%Y-%m-%d").date()  # Convert to date object
+                issue_date_input = input("Enter issue date (YYYY-MM-DD): ")
+                issue_date = datetime.strptime(issue_date_input, "%Y-%m-%d").date()
+                due_date_input = input("Enter due date (YYYY-MM-DD): ")
+                due_date = datetime.strptime(due_date_input, "%Y-%m-%d").date()
+                transaction_status = input("Enter transaction status (Active/Completed/Overdue): ")
+                notes = input("Enter any additional notes: ")
+                
+                fine_amount = add_transaction(connection, member_id, book_id, transaction_type, issue_date, due_date, return_date, transaction_status, notes)
+                
+                if fine_amount is not None:
+                    print(f"The fine amount is: Ksh{fine_amount:.2f}")
+           
+        elif choice == '4':
             print("Exiting program")
             break
         
